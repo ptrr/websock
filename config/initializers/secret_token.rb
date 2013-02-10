87b724dce1692198b30f51e104e0597d00f7375f
Peter de Ruijter <peterderuijter@Peters-MacBook-Pro.local>
2013-02-10 21:08:47 +0100

    Boom, headshot.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebsocketConcept::Application.config.secret_token = '55b9079a9f0cd2e68b97f3beea52006e8fdf6e535730e8dd51bc07f9e3768ceeb00d7b267d7d094feb7c635cd4d5bb1776c35096edf3d52440b6ab9ae9b6a192'
